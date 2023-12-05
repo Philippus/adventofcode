806c9bcc543e01118cb81e3a9fdb09089ae10363
philippus <philippus@gmail.com>
2023-12-06 00:52:47 +0100

    Add day 7 of 2015

---
+++ src/main/scala/adventofcode2015/Day7.scala
@@ -0,0 +1,79 @@
+package adventofcode2015
+
+import scala.annotation.tailrec
+import scala.io.Source
+import scala.util.{Try, Using}
+
+object Day7:
+  case class Wire(id: String, signal: Int)
+
+  @tailrec
+  def runConnection(connections: Seq[String], wires: Set[Wire] = Set.empty[Wire]): Set[Wire] =
+    if connections.isEmpty then
+      wires
+    else
+      val connection = connections.head
+      connection match
+        case s"$x AND $y -> $d" if x.toIntOption.nonEmpty =>
+          wires.find(_.id == y) match
+            case Some(wire) =>
+              runConnection(connections.tail, wires + Wire(d, x.toInt & wire.signal))
+            case _          =>
+              runConnection(connections.tail :+ connection, wires)
+        case s"$x AND $y -> $d"                           =>
+          (wires.find(_.id == x), wires.find(_.id == y)) match
+            case (Some(wirex), Some(wirey)) =>
+              runConnection(connections.tail, wires + Wire(d, wirex.signal & wirey.signal))
+            case _                          =>
+              runConnection(connections.tail :+ connection, wires)
+        case s"$x OR $y -> $e" if x.toIntOption.nonEmpty  =>
+          wires.find(_.id == y) match
+            case Some(wire) =>
+              runConnection(connections.tail, wires + Wire(e, x.toInt | wire.signal))
+            case _          =>
+              runConnection(connections.tail :+ connection, wires)
+        case s"$x OR $y -> $e"                            =>
+          (wires.find(_.id == x), wires.find(_.id == y)) match
+            case (Some(wirex), Some(wirey)) =>
+              runConnection(connections.tail, wires + Wire(e, wirex.signal | wirey.signal))
+            case _                          =>
+              runConnection(connections.tail :+ connection, wires)
+        case s"$x LSHIFT $i -> $e"                        =>
+          wires.find(_.id == x) match
+            case Some(wirex) =>
+              runConnection(connections.tail, wires + Wire(e, wirex.signal << i.toInt))
+            case _           =>
+              runConnection(connections.tail :+ connection, wires)
+        case s"$x RSHIFT $i -> $e"                        =>
+          wires.find(_.id == x) match
+            case Some(wirex) =>
+              runConnection(connections.tail, wires + Wire(e, wirex.signal >> i.toInt))
+            case _           =>
+              runConnection(connections.tail :+ connection, wires)
+        case s"NOT $x -> $e"                              =>
+          wires.find(_.id == x) match
+            case Some(wirex) =>
+              runConnection(connections.tail, wires + Wire(e, ~wirex.signal))
+            case _           =>
+              runConnection(connections.tail :+ connection, wires)
+        case s"$x -> $w" if x.toIntOption.nonEmpty        =>
+          runConnection(connections.tail, wires + Wire(w, x.toInt))
+        case s"$x -> $w"                                  =>
+          wires.find(_.id == x) match
+            case Some(wirex) =>
+              runConnection(connections.tail, wires + Wire(w, wirex.signal))
+            case _           =>
+              runConnection(connections.tail :+ connection, wires)
+        case _                                            =>
+          runConnection(connections.tail :+ connection, wires)
+
+  def runCircuit: Int =
+    Using.resource(Source.fromResource("2015/day7input.txt")): source =>
+      val lines = source.getLines().toSeq
+      runConnection(lines, Set.empty[Wire]).find(_.id == "a").get.signal
+
+  def runCircuitAfterRewiring: Int =
+    Using.resource(Source.fromResource("2015/day7input.txt")): source =>
+      val lines = source.getLines().toSeq
+      runConnection(lines, Set(Wire("b", runCircuit))).find(_.id == "a").get.signal
+end Day7
---
+++ src/test/scala/adventofcode2015/Day7Suite.scala
@@ -0,0 +1,27 @@
+package adventofcode2015
+
+import scala.io.Source
+import scala.util.Using
+
+import adventofcode2015.Day7.*
+import munit.FunSuite
+
+class Day7Suite extends FunSuite:
+  test("test signal"):
+    assertEquals(runConnection(Seq("123 -> x")), Set(Wire("x", 123)))
+
+  test("small diagram"):
+    assertEquals(runConnection(Seq("123 -> x", "456 -> y", "x AND y -> d")).find(_.id == "d").get, Wire("d", 72))
+
+  test("simple circuit"):
+    Using.resource(Source.fromResource("2015/day7sampleinput.txt")): source =>
+      val lines = source.getLines().toSeq
+      val h     = runConnection(lines).find(_.id == "h").get.signal
+      assertEquals(((h % 65536) + 65536) % 65536, 65412)
+
+  test("calculates signal on wire a for input file"):
+    assertEquals(runCircuit, 3176)
+
+  test("calculates signal on wire a after rewiring in part two"):
+    assertEquals(runCircuitAfterRewiring, 14710)
+end Day7Suite
