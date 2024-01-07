cc4f83016a4df0bfb33c1176f19bf0cee3fe1162
philippus <philippus@gmail.com>
2024-01-07 16:04:33 +0100

    Run scalafmt

--- src/main/scala/adventofcode2015/Day24.scala
+++ src/main/scala/adventofcode2015/Day24.scala
@@ -7,10 +7,12 @@ object Day24:
   def determineMinimumCombinations(packages: Seq[BigInt]): BigInt =
     val weight = packages.sum / 3
     (for
-      combination <- packages.combinations(14).filter(_.sum == weight) // determined that the biggest group contains 14 packages
-      j <- 1 to 14
-      combinationTwo <- packages.diff(combination).combinations(j).filter(_.sum == weight)
-      combinationThree = packages.diff(combination).diff(combinationTwo) // last group's weight doesn't need to be checked
+      combination     <-
+        packages.combinations(14).filter(_.sum == weight) // determined that the biggest group contains 14 packages
+      j               <- 1 to 14
+      combinationTwo  <- packages.diff(combination).combinations(j).filter(_.sum == weight)
+      combinationThree =
+        packages.diff(combination).diff(combinationTwo) // last group's weight doesn't need to be checked
     yield Seq(combination, combinationTwo, combinationThree).minBy(_.length)).minBy(x =>
       (x.length, x.product)
     ).product
@@ -18,12 +20,16 @@ object Day24:
   def determineMinimumCombinationsForPartTwo(packages: Seq[BigInt]): BigInt =
     val weight = packages.sum / 4
     (for
-      combination      <- packages.combinations(13).filter(_.sum == weight) // determined that the biggest group contains 13 packages
+      combination      <-
+        packages.combinations(13).filter(_.sum == weight) // determined that the biggest group contains 13 packages
       j                <- 1 to 13
       combinationTwo   <- packages.diff(combination).combinations(j).filter(_.sum == weight)
       k                <- 1 to 13
       combinationThree <- packages.diff(combination).diff(combinationTwo).combinations(k).filter(_.sum == weight)
-      combinationFour   = packages.diff(combination).diff(combinationTwo).diff(combinationThree)  // last group's weight doesn't need to be checked
+      combinationFour   =
+        packages.diff(combination).diff(combinationTwo).diff(
+          combinationThree
+        ) // last group's weight doesn't need to be checked
     yield Seq(combination, combinationTwo, combinationThree, combinationFour).minBy(_.length)).minBy(x =>
       (x.length, x.product)
     ).product
