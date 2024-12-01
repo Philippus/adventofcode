f993ef3a3728cb7c491a1f77d0f9753388077f01
philippus <philippus@gmail.com>
2024-12-01 06:40:50 +0100

    Add day 1 of 2024

--- src/main/scala/adventofcode2024/Day01.scala
+++ src/main/scala/adventofcode2024/Day01.scala
@@ -1,12 +1,26 @@
 package adventofcode2024
 
-import scala.annotation.tailrec
 import scala.io.Source
 import scala.util.Using
 
 object Day01:
+  private def createListsFromInput(lines: Seq[String]): (Seq[Int], Seq[Int]) =
+    val seqTuples: Seq[(Int, Int)] = lines.map(_.split("\\s+")).map {
+      case Array(a, b) =>
+        (a.toInt, b.toInt)
+      case _           => (0, 0)
+    }
+    (seqTuples.map(_._1), seqTuples.map(_._2))
 
-  def importLines(): Seq[Int] =
+  def distanceBetweenLists(lines: Seq[String]): Int =
+    val (left, right) = createListsFromInput(lines)
+    left.sorted.zip(right.sorted).map((l, r) => scala.math.abs(r - l)).sum
+
+  def similarityBetweenLists(lines: Seq[String]): Int =
+    val (left, right) = createListsFromInput(lines)
+    left.map(l => right.count(_ == l) * l).sum
+
+  def importLines(): Seq[String] =
     Using.resource(Source.fromResource("2024/day01input.txt")): source =>
-      source.getLines().toSeq.map(_.toInt)
+      source.getLines().toSeq
 end Day01
--- src/test/scala/adventofcode2024/Day01Suite.scala
+++ src/test/scala/adventofcode2024/Day01Suite.scala
@@ -1,9 +1,25 @@
 package adventofcode2024
 
+import scala.io.Source
+import scala.util.Using
+
 import adventofcode2024.Day01.*
 import munit.FunSuite
 
 class Day01Suite extends FunSuite:
-  test("calculates"):
-    assertEquals(0, 1)
+  test("calculates total distance between lists for the sample"):
+    assertEquals(distanceBetweenLists(importSampleLines()), 11)
+
+  test("calculates total distance between lists"):
+    assertEquals(distanceBetweenLists(importLines()), 2367773)
+
+  test("calculates similarity score between lists for the sample"):
+    assertEquals(similarityBetweenLists(importSampleLines()), 31)
+
+  test("calculates similarity score between lists"):
+    assertEquals(similarityBetweenLists(importLines()), 21271939)
+
+  def importSampleLines(): Seq[String] =
+    Using.resource(Source.fromResource("2024/day01sampleinput.txt")): source =>
+      source.getLines().toSeq
 end Day01Suite
