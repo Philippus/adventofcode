f727ff6bb4b9200008457b43da97b9e76a7dffd3
philippus <philippus@gmail.com>
2024-12-14 17:12:25 +0100

    Add day 15 of 2023 part 2

--- src/main/scala/adventofcode2023/Day15.scala
+++ src/main/scala/adventofcode2023/Day15.scala
@@ -1,9 +1,14 @@
 package adventofcode2023
 
+import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 import scala.util.Using
 
 object Day15:
+  case class Lens(label: String, focalLength: Long)
+
+  case class Box(lenses: Seq[Lens])
+
   def calculateHash(hash: String): Long =
     var currentValue = 0L
     hash.foreach: char =>
@@ -14,6 +19,45 @@ object Day15:
 
   def sumOfHashes(steps: String): Long =
     steps.split(',').toSeq.map(calculateHash).sum
+
+  def fillBoxes(steps: String): Seq[Box] =
+    val boxes = new ArrayBuffer[Box](256)
+    for
+      i <- 0 until 256
+    do
+      boxes += Box(Seq.empty[Lens])
+
+    steps.split(',').toSeq.foreach:
+      case s"$label=$focalLength" =>
+        val boxId = calculateHash(label).toInt
+        val box   = boxes(boxId)
+        if box.lenses.exists(lens => lens.label == label) then
+          boxes.update(
+            boxId,
+            box.copy(lenses =
+              box.lenses.updated(box.lenses.indexWhere(lens => lens.label == label), Lens(label, focalLength.toLong))
+            )
+          )
+        else
+          boxes.update(
+            boxId,
+            box.copy(lenses =
+              box.lenses :+ Lens(label, focalLength.toLong)
+            )
+          )
+      case s"$label-"             =>
+        val boxId = calculateHash(label).toInt
+        val box   = boxes(boxId)
+        boxes.update(boxId, box.copy(lenses = box.lenses.filterNot(lens => lens.label == label)))
+    boxes.toSeq
+
+  def calculateFocusingPower(boxes: Seq[Box]): Long =
+    (for
+      (box, i) <- boxes.zipWithIndex
+      if box.lenses.nonEmpty
+    yield (i + 1) * (box.lenses.zipWithIndex.map:
+      case (lens, j) => (j + 1) * lens.focalLength
+    ).sum).sum
 
   def importLines(): String =
     Using.resource(Source.fromResource(s"2023/${this.getClass.getSimpleName.toLowerCase.replace("$", "")}input.txt")):
--- src/test/scala/adventofcode2023/Day15Suite.scala
+++ src/test/scala/adventofcode2023/Day15Suite.scala
@@ -12,4 +12,16 @@ class Day15Suite extends FunSuite:
 
   test("calculates sum of hashes for the input"):
     assertEquals(sumOfHashes(importLines()), 504449L)
+
+  test("fills boxes for the sample"):
+    val boxes = fillBoxes("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7")
+    assertEquals(boxes.head, Box(Seq(Lens("rn", 1L), Lens("cm", 2L))))
+    assertEquals(boxes(3), Box(Seq(Lens("ot", 7L), Lens("ab", 5L), Lens("pc", 6L))))
+
+  test("calculates focusing power for the sample"):
+    val boxes = fillBoxes("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7")
+    assertEquals(calculateFocusingPower(boxes), 145L)
+
+  test("calculates focusing power for the input"):
+    assertEquals(calculateFocusingPower(fillBoxes(importLines())), 145L)
 end Day15Suite
