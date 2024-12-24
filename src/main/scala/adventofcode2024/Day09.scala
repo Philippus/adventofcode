a10ab0938b38bfe6c52b163cf0459f0072963d9c
philippus <philippus@gmail.com>
2024-12-24 12:46:53 +0100

    Add day 9 of 2024

---
+++ src/main/scala/adventofcode2024/Day09.scala
@@ -0,0 +1,58 @@
+package adventofcode2024
+
+import scala.annotation.tailrec
+import scala.io.Source
+import scala.util.Using
+
+object Day09:
+  def createBlocks(diskmap: String): Seq[Int] =
+    diskmap.grouped(2).zipWithIndex.toSeq.flatMap:
+      case s if s._1.length == 1 => Seq.fill(s._1.toInt)(s._2)
+      case s                     => Seq.fill(s._1.head.asDigit)(s._2) ++ Seq.fill(s._1.last.asDigit)(-1)
+
+  @tailrec
+  def moveBlocks(blocks: Seq[Int]): Seq[Int] =
+    if blocks.indexWhere(_.==(-1)) > blocks.lastIndexWhere(!_.==(-1)) then
+      blocks
+    else
+      moveBlocks(blocks
+        .updated(blocks.indexWhere(_.==(-1)), blocks(blocks.lastIndexWhere(!_.==(-1))))
+        .updated(blocks.lastIndexWhere(!_.==(-1)), -1))
+
+  def moveBlocksPartTwo(blocks: Seq[Int]): Seq[Int] =
+    @tailrec
+    def loop(blocks: Seq[Int], files: Seq[Int]): Seq[Int] =
+      if files.isEmpty then
+        blocks
+      else
+        val fiw          = blocks.indexWhere(_.==(files.last))
+        val liw          = blocks.lastIndexWhere(_.==(files.last))
+        val lengthOfFile = liw - fiw + 1
+        blocks.indexOfSlice(Seq.fill(lengthOfFile)(-1)) match
+          case -1           =>
+            loop(blocks, files.init)
+          case i if i < liw =>
+            loop(
+              blocks.take(i) ++
+                Seq.fill(lengthOfFile)(files.last) ++
+                blocks.slice(i + lengthOfFile, fiw) ++
+                Seq.fill(lengthOfFile)(-1) ++
+                blocks.drop(liw + 1),
+              files.init
+            )
+          case _            =>
+            loop(blocks, files.init)
+
+    val files = blocks.distinct.filterNot(_.==(-1))
+    loop(blocks, files)
+
+  def fileChecksum(blocks: Seq[Int]): Long =
+    blocks.zipWithIndex.map:
+      case (block, index) => if (block != -1) then index.toLong * block.toLong else 0L
+    .sum
+
+  def importLines(): String =
+    Using.resource(Source.fromResource(s"2024/${this.getClass.getSimpleName.toLowerCase.replace("$", "")}input.txt")):
+      source =>
+        source.getLines().next()
+end Day09
---
+++ src/test/scala/adventofcode2024/Day09Suite.scala
@@ -0,0 +1,51 @@
+package adventofcode2024
+
+import scala.concurrent.duration.*
+import scala.io.Source
+import scala.util.Using
+
+import adventofcode2024.Day09.*
+import munit.FunSuite
+
+class Day09Suite extends FunSuite:
+  override val munitTimeout = Duration(2, "d")
+  test("prints blocks"):
+    val diskmap = importSampleLines()
+    val blocks  = createBlocks(diskmap)
+    println(blocks.map(i => if i == -1 then "." else i.toString).mkString)
+
+  test("moves blocks for the sample"):
+    val diskmap     = importSampleLines()
+    val blocks      = createBlocks(diskmap)
+    val movedBlocks = moveBlocks(blocks)
+    val checksum    = fileChecksum(movedBlocks)
+    assertEquals(checksum, 1928L)
+
+  test("moves blocks for the input"):
+    val diskmap     = importLines()
+    val blocks      = createBlocks(diskmap)
+    val movedBlocks = moveBlocks(blocks)
+    val checksum    = fileChecksum(movedBlocks)
+    assertEquals(checksum, 6384282079460L)
+
+  test("moves whole files for the sample"):
+    val diskmap     = importSampleLines()
+    val blocks      = createBlocks(diskmap)
+    val movedBlocks = moveBlocksPartTwo(blocks)
+    val checksum    = fileChecksum(movedBlocks)
+    assertEquals(checksum, 2858L)
+
+  // warning: this takes forever, but it works
+  test("moves files for the input"):
+    val diskmap     = importLines()
+    val blocks      = createBlocks(diskmap)
+    val movedBlocks = moveBlocksPartTwo(blocks)
+    val checksum    = fileChecksum(movedBlocks)
+    assertEquals(checksum, 6408966547049L)
+
+  def importSampleLines(): String =
+    Using.resource(
+      Source.fromResource(s"2024/${this.getClass.getSimpleName.toLowerCase.replace("suite", "")}sampleinput.txt")
+    ): source =>
+      source.getLines().next()
+end Day09Suite
