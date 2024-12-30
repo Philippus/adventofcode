e98d218062a00092d71a3e5fc8544b9970db6a17
philippus <philippus@gmail.com>
2024-12-30 08:19:03 +0100

    Add day 13 of 2020 part 2

--- src/main/scala/adventofcode2020/Day13.scala
+++ src/main/scala/adventofcode2020/Day13.scala
@@ -1,14 +1,51 @@
 package adventofcode2020
 
+import scala.annotation.tailrec
 import scala.io.Source
 import scala.util.Using
 
 object Day13:
+  // https://rosettacode.org/wiki/Chinese_remainder_theorem
+  def chineseRemainder(n: List[Long], a: List[Long]): Long =
+    val prod = n.product
+
+    @tailrec
+    def iter(n: List[Long], a: List[Long], sm: Long): Long =
+      def mulInv(a: Long, b: Long): Long =
+        @tailrec
+        def loop(a: Long, b: Long, x0: Long, x1: Long): Long =
+          if (a > 1) loop(b, a % b, x1 - (a / b) * x0, x0) else x1
+
+        if b == 1 then
+          1
+        else
+          val x1 = loop(a, b, 0, 1)
+          if x1 < 0 then x1 + b else x1
+
+      if n.nonEmpty then
+        val p = prod / n.head
+        iter(n.tail, a.tail, sm + a.head * mulInv(p, n.head) * p)
+      else
+        sm
+
+    iter(n, a, 0) % prod
+
   def determineEarliestBus(timestamp: Long, buses: Seq[Int]): Long =
     val (bus, wait) = buses.map: bus =>
       (bus, bus - (timestamp % bus))
     .minBy(_._2)
     bus * wait
+
+  def determineEarliestTimestamp(buses: Seq[(Long, Long)]): Long =
+    val modulos    = buses.map(_._1)
+    val remainders = buses.map(bus => bus._1 - bus._2)
+    chineseRemainder(modulos.toList, remainders.toList)
+
+  def importLinesPartTwo(): Seq[(Long, Long)] =
+    Using.resource(Source.fromResource(s"2020/${this.getClass.getSimpleName.toLowerCase.replace("$", "")}input.txt")):
+      source =>
+        val list = source.getLines().toList
+        list.last.split(',').zipWithIndex.filterNot(_._1 == ("x")).map(x => (x._1.toLong, x._2.toLong))
 
   def importLines(): (Long, Seq[Int]) =
     Using.resource(Source.fromResource(s"2020/${this.getClass.getSimpleName.toLowerCase.replace("$", "")}input.txt")):
--- src/test/scala/adventofcode2020/Day13Suite.scala
+++ src/test/scala/adventofcode2020/Day13Suite.scala
@@ -15,6 +15,21 @@ class Day13Suite extends FunSuite:
     val (timestamp, buses) = importLines()
     assertEquals(determineEarliestBus(timestamp, buses), 203L)
 
+  test("determines earliest timestamp with offset departures for the sample"):
+    val buses = importSampleLinesPartTwo()
+    assertEquals(determineEarliestTimestamp(buses), 1068781L)
+
+  test("determines earliest timestamp with offset departures for the input"):
+    val buses = importLinesPartTwo()
+    assertEquals(determineEarliestTimestamp(buses), 905694340256752L)
+
+  def importSampleLinesPartTwo(): Seq[(Long, Long)] =
+    Using.resource(
+      Source.fromResource(s"2020/${this.getClass.getSimpleName.toLowerCase.replace("suite", "")}sampleinput.txt")
+    ): source =>
+      val list = source.getLines().toList
+      list.last.split(',').zipWithIndex.filterNot(_._1 == ("x")).map(x => (x._1.toLong, x._2.toLong))
+
   def importSampleLines(): (Long, Seq[Int]) =
     Using.resource(
       Source.fromResource(s"2020/${this.getClass.getSimpleName.toLowerCase.replace("suite", "")}sampleinput.txt")
