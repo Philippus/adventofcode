955cce2c2fc32c06c691aaede67f54eb405039a5
philippus <philippus@gmail.com>
2025-01-11 01:24:38 +0100

    Add day 14 of 2016 part 2

--- src/main/scala/adventofcode2016/Day14.scala
+++ src/main/scala/adventofcode2016/Day14.scala
@@ -1,6 +1,9 @@
 package adventofcode2016
 
 import java.security.MessageDigest
+import scala.collection.mutable
+import scala.io.Source
+import scala.util.Using
 
 object Day14:
   val messageDigest: MessageDigest = MessageDigest.getInstance("MD5")
@@ -12,7 +15,7 @@ object Day14:
       hex.append(String.format("%02x", b))
     hex.toString
 
-  def stretchedHash(s: String): String   =
+  def stretchedHash(s: String): String =
     var i           = 0
     val initialHash = md5(s)
     var hash        = initialHash
@@ -20,22 +23,29 @@ object Day14:
       hash = md5(hash)
       i += 1
     hash
+
   def hasTriple(s: String): Option[Char] =
     s.sliding(3).find(_.distinct.length == 1).map(_.head)
 
   def hasQuintuple(s: String, c: Char): Boolean =
     s.sliding(5).exists(s => s.forall(_.==(c)))
 
-  def determineIndexOf64thKey(line: String): Int =
+  def determineIndexOf64thKey(line: String, stretched: Boolean = false): Int =
+    val map = mutable.SortedMap[String, String]()
+
+    def md5Memo(s: String): String = map.getOrElseUpdate(s, md5(s))
+
+    def stretchedHashMemo(s: String): String = map.getOrElseUpdate(s, stretchedHash(s))
+
     var i: Int       = 0
     var indexesFound = 0
     while (indexesFound < 64)
-      hasTriple(md5(s"$line$i")) match
+      hasTriple(if stretched then stretchedHashMemo(s"$line$i") else md5Memo(s"$line$i")) match
         case Some(c) =>
           var j          = i + 1
           var indexFound = false
           while (j - i < 1001 && !indexFound)
-            if (hasQuintuple(md5(s"$line$j"), c))
+            if (hasQuintuple(if stretched then stretchedHashMemo(s"$line$j") else md5Memo(s"$line$j"), c))
               indexFound = true
               indexesFound += 1
             j += 1
@@ -43,21 +53,8 @@ object Day14:
       i += 1
     i - 1
 
-  def determineIndexOf64thKeyPartTwo(line: String): Int =
-    var i: Int       = 0
-    var indexesFound = 0
-    while (indexesFound < 64)
-      hasTriple(stretchedHash(s"$line$i")) match
-        case Some(c) =>
-          var j          = i + 1
-          var indexFound = false
-          while (j - i < 1001 && !indexFound)
-            if (hasQuintuple(stretchedHash(s"$line$j"), c))
-              indexFound = true
-              indexesFound += 1
-              println(s"$indexesFound for $i")
-            j += 1
-        case None    =>
-      i += 1
-    i - 1
+  def importLines(): String =
+    Using.resource(Source.fromResource(s"2016/${this.getClass.getSimpleName.toLowerCase.replace("$", "")}input.txt")):
+      source =>
+        source.getLines().next()
 end Day14
--- src/test/scala/adventofcode2016/Day14Suite.scala
+++ src/test/scala/adventofcode2016/Day14Suite.scala
@@ -1,9 +1,12 @@
 package adventofcode2016
 
+import scala.concurrent.duration.Duration
+
 import adventofcode2016.Day14.*
 import munit.FunSuite
 
 class Day14Suite extends FunSuite:
+  override val munitTimeout = Duration(1000, "s")
   test("generates hashes"):
     assertEquals(md5("abc18"), "0034e0923cc38887a57bd7b1d4f953df")
     assertEquals(md5("abc39"), "347dac6ee8eeea4652c7476d0f97bee5")
@@ -16,17 +19,18 @@ class Day14Suite extends FunSuite:
   test("determines quintuples"):
     assert(hasQuintuple("3aeeeee1367614f3061d165a5fe3cac3", 'e'))
 
+  test("determines index of 64th key"):
+    assertEquals(determineIndexOf64thKey("abc"), 22728)
+    assertEquals(determineIndexOf64thKey(importLines()), 25427)
+
   test("determines stretched hash"):
     assertEquals(stretchedHash("abc0"), "a107ff634856bb300138cac6568c0f24")
     assertEquals(stretchedHash("abc10"), "4a81e578d9f43511ab693eee1a75f194")
     assertEquals(stretchedHash("abc89"), "eaa5c17bec47565b98275b404eeeeea6")
     assertEquals(stretchedHash("abc22551"), "2df6e9378c3c53abed6d3508b6285fff")
     assertEquals(stretchedHash("abc22859"), "2e559978fffff9ac9c9012eb764c6391")
-//  test("determines index of 64th key"):
-//    assertEquals(determineIndexOf64thKey("abc"), 22728)
-  //    assertEquals(determineIndexOf64thKey("yjdafjpo"), 25427)
 
-  test("determines index of 64th key part two"):
-//    assertEquals(determineIndexOf64thKeyPartTwo("abc"), 22551)
-    assertEquals(determineIndexOf64thKeyPartTwo("yjdafjpo"), 25427)
+  test("determines index of 64th key with key stretching"):
+    assertEquals(determineIndexOf64thKey("abc", stretched = true), 22551)
+    assertEquals(determineIndexOf64thKey(importLines(), stretched = true), 22045)
 end Day14Suite
