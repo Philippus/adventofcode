0189383dce91ce6abfdce4dd9cf4d0e4814be271
philippus <philippus@gmail.com>
2025-02-19 17:41:13 +0100

    Add day 7 of 2018 part 2

--- src/main/scala/adventofcode2018/Day07.scala
+++ src/main/scala/adventofcode2018/Day07.scala
@@ -1,6 +1,7 @@
 package adventofcode2018
 
 import scala.annotation.tailrec
+import scala.collection.mutable
 import scala.io.Source
 import scala.util.Using
 
@@ -21,6 +22,40 @@ object Day07:
     val available      = lefts.distinct.diff(rights.distinct)
     val result         = handle(available, "", steps)
     result ++ rights.distinct.diff(result)
+
+  def followInstructionsWithMultipleWorkers(steps: Seq[String], workers: Int, offset: Int): (String, Int) =
+    val workerMap: mutable.Map[Int, Option[(Char, Int)]] = (for
+      i <- 1 to workers
+    yield i -> None).to(mutable.Map)
+
+    var done = ""
+
+    @tailrec
+    def handlePt2(steps: Seq[String], tick: Int): (String, Int) =
+      workerMap.foreach:
+        case (i, None)                           => ()
+        case (i, Some((c, ticks))) if ticks == 1 =>
+          done += s"$c"
+          workerMap.update(i, None)
+        case (i, Some((c, ticks)))               =>
+          workerMap.update(i, Some((c, ticks - 1)))
+      val lefts: String  = steps.filterNot(step => done.contains(step.head)).map(_.head).mkString.distinct
+      val rights: String = steps.filterNot(step => done.contains(step.head)).map(_.last).mkString.distinct
+      val onlyAtRight    = steps.map(_.last).filterNot(c => steps.map(_.head).contains(c)).mkString.distinct
+      var available      =
+        lefts.distinct.diff(rights.distinct).filterNot(c => workerMap.exists(_._2.exists(_._1 == c))).sorted
+      if available.isEmpty && workerMap.forall(_._2.isEmpty) then available = onlyAtRight
+      if done.contains(onlyAtRight) && workerMap.forall(_._2.isEmpty) then
+        (done, tick)
+      else
+        while available.nonEmpty && workerMap.exists(_._2.isEmpty) do
+          val process = available.head
+          workerMap.find(_._2.isEmpty).foreach: freeWorker =>
+            workerMap.update(freeWorker._1, Some(available.head, available.head.toInt - 64 + offset))
+          available = available.tail
+        handlePt2(steps, tick + 1)
+
+    handlePt2(steps, 0)
 
   def readSteps(steps: Seq[String]): Seq[String] =
     steps.map:
--- src/test/scala/adventofcode2018/Day07Suite.scala
+++ src/test/scala/adventofcode2018/Day07Suite.scala
@@ -1,6 +1,5 @@
 package adventofcode2018
 
-import scala.concurrent.duration.Duration
 import scala.io.Source
 import scala.util.Using
 
@@ -15,6 +14,18 @@ class Day07Suite extends FunSuite:
   test("reacts polymer for the input"):
     val result = bootstrap(readSteps(importLines()))
     assertEquals(result, "HEGMPOAWBFCDITVXYZRKUQNSLJ")
+
+  test("completes steps with 1 workers for the sample"):
+    assertEquals(followInstructionsWithMultipleWorkers(readSteps(importSampleLines()), 1, 0)._1, "CABDFE")
+
+  test("completes steps with 1 workers for the input"):
+    assertEquals(followInstructionsWithMultipleWorkers(readSteps(importLines()), 1, 0)._1, "HEGMPOAWBFCDITVXYZRKUQNSLJ")
+
+  test("completes steps with 2 workers for the sample"):
+    assertEquals(followInstructionsWithMultipleWorkers(readSteps(importSampleLines()), 2, 0)._2, 15)
+
+  test("completes steps with 5 workers for the input"):
+    assertEquals(followInstructionsWithMultipleWorkers(readSteps(importLines()), 5, 60)._2, 1226)
 
   def importSampleLines(): Seq[String] =
     Using.resource(Source.fromResource("2018/day07sampleinput.txt")):
