92586ed11f0800da39b6c66a70301f79ac17ffc1
philippus <philippus@gmail.com>
2025-12-06 22:45:27 +0100

    Add day 12 of 2019 part 2

--- src/main/scala/adventofcode2019/Day12.scala
+++ src/main/scala/adventofcode2019/Day12.scala
@@ -15,17 +15,76 @@ object Day12:
     else
       val combos = moons.combinations(2)
 
-      val diffs = combos.flatMap {
+      val diffs        = combos.flatMap {
         case m1 :: m2 :: _ =>
-          List((m1.id, (m2.x - m1.x).sign, (m2.y - m1.y).sign, (m2.z - m1.z).sign), (m2.id, (m1.x - m2.x).sign, (m1.y - m2.y).sign, (m1.z - m2.z).sign))
+          List(
+            (m1.id, (m2.x - m1.x).sign, (m2.y - m1.y).sign, (m2.z - m1.z).sign),
+            (m2.id, (m1.x - m2.x).sign, (m1.y - m2.y).sign, (m1.z - m2.z).sign)
+          )
       }.toList
       val updatedMoons = moons.map: moon =>
         val diffX = diffs.filter(_._1 == moon.id).map(_._2).sum
         val diffY = diffs.filter(_._1 == moon.id).map(_._3).sum
         val diffZ = diffs.filter(_._1 == moon.id).map(_._4).sum
-        val newMoon = moon.copy(velX = moon.velX + diffX, velY = moon.velY + diffY, velZ = moon.velZ + diffZ)
-        newMoon.copy(x = newMoon.x + newMoon.velX, y = newMoon.y + newMoon.velY, z = newMoon.z + newMoon.velZ)
+        moon.copy(
+          x = moon.x + moon.velX + diffX,
+          y = moon.y + moon.velY + diffY,
+          z = moon.z + moon.velZ + diffZ,
+          velX = moon.velX + diffX,
+          velY = moon.velY + diffY,
+          velZ = moon.velZ + diffZ
+        )
       calculateEnergy(updatedMoons, cycles - 1)
+
+  @tailrec
+  def findCyclesOfMoons(
+      moons: List[Moon],
+      cycles: Int,
+      initialMoons: List[Moon],
+      currentMoons: List[Moon],
+      found: (Int, Int, Int)
+  ): BigInt = {
+    if found._1 > 0 && found._2 > 0 && found._3 > 0 then
+      lcm(lcm(BigInt(found._1), BigInt(found._2)), BigInt(found._3))
+    else if found._1 == 0 && initialMoons.map(m => (m.x, m.velX)) == currentMoons.map(m => (m.x, m.velX)) && cycles > 0
+    then
+      findCyclesOfMoons(moons, cycles, initialMoons, currentMoons, found.copy(_1 = cycles))
+    else if found._2 == 0 && initialMoons.map(m => (m.y, m.velY)) == currentMoons.map(m => (m.y, m.velY)) && cycles > 0
+    then
+      findCyclesOfMoons(moons, cycles, initialMoons, currentMoons, found.copy(_2 = cycles))
+    else if found._3 == 0 && initialMoons.map(m => (m.z, m.velZ)) == currentMoons.map(m => (m.z, m.velZ)) && cycles > 0
+    then
+      findCyclesOfMoons(moons, cycles, initialMoons, currentMoons, found.copy(_3 = cycles))
+    else
+      val combos       = moons.combinations(2)
+      val diffs        = combos.flatMap:
+        case m1 :: m2 :: _ =>
+          List(
+            (m1.id, (m2.x - m1.x).sign, (m2.y - m1.y).sign, (m2.z - m1.z).sign),
+            (m2.id, (m1.x - m2.x).sign, (m1.y - m2.y).sign, (m1.z - m2.z).sign)
+          )
+      .toList
+      val updatedMoons = moons.map: moon =>
+        val diffX = diffs.filter(_._1 == moon.id).map(_._2).sum
+        val diffY = diffs.filter(_._1 == moon.id).map(_._3).sum
+        val diffZ = diffs.filter(_._1 == moon.id).map(_._4).sum
+        moon.copy(
+          x = moon.x + moon.velX + diffX,
+          y = moon.y + moon.velY + diffY,
+          z = moon.z + moon.velZ + diffZ,
+          velX = moon.velX + diffX,
+          velY = moon.velY + diffY,
+          velZ = moon.velZ + diffZ
+        )
+      findCyclesOfMoons(updatedMoons, cycles + 1, initialMoons, updatedMoons, found)
+  }
+
+  def lcm(a: BigInt, b: BigInt): BigInt =
+    @tailrec
+    def gdc(a: BigInt, b: BigInt): BigInt =
+      if b == 0 then a else gdc(b, a % b)
+
+    a * (b / gdc(a, b))
 
   def parse(input: String): List[Moon] =
     input.split('\n').zipWithIndex.map:
--- src/test/scala/adventofcode2019/Day12Suite.scala
+++ src/test/scala/adventofcode2019/Day12Suite.scala
@@ -8,6 +8,16 @@ import adventofcode2019.Day12.*
 import munit.FunSuite
 
 class Day12Suite extends FunSuite:
+  test("finds cycles when moons are back to initial state for the sample"):
+    val input = importSampleLines()
+    val moons = parse(input)
+    assertEquals(findCyclesOfMoons(moons, 0, moons, List.empty[Moon], (0, 0, 0)), BigInt(2772))
+
+  test("finds cycles when moons are back to initial state for the input"):
+    val input = importLines()
+    val moons = parse(input)
+    assertEquals(findCyclesOfMoons(moons, 0, moons, List.empty[Moon], (0, 0, 0)), BigInt(551272644867044L))
+
   test("calculates energy of the system for the sample"):
     val input = importSampleLines()
     assertEquals(calculateEnergy(parse(input), 10), 179)
